f22ba69ab52cbdb114b32678c8f98f769013b7a5
Casper Guo <casperg@umich.edu>
2025-09-28 20:50:01 -0500

    Simplify shared logic in integration scripts
    Moved get_venue_info to util and simplified internal data model creation

--- trip_solver/data/integration/mlb/integrate.py
+++ trip_solver/data/integration/mlb/integrate.py
@@ -3,40 +3,13 @@
 import logging
 from pathlib import Path
 
-from pydantic import ValidationError
-
-from trip_solver.data.api.google_maps.places import TextSearch
 from trip_solver.data.api.mlb.schedule import MLBSchedule
 from trip_solver.data.api.mlb.teams import MLBTeams
-from trip_solver.models.api.google_maps.places import TextSearchRequestBody
-from trip_solver.models.internal import Event, Events, Team, Teams, Venue, Venues
+from trip_solver.data.integration import get_venue_info
+from trip_solver.models.internal import Event, Events, Team, Teams, Venues
 
 logging.basicConfig(level=logging.INFO, format="%(filename)s\t%(levelname)s\t%(message)s")
 logger = logging.getLogger(__name__)
-
-
-def get_venue_info(venue_full_name: str, venue_id: int) -> Venue:
-    """Format the response from the Google Maps Places API."""
-    try:
-        response = (
-            TextSearch()
-            .post_for_data(
-                request_body=TextSearchRequestBody(textQuery=venue_full_name),
-            )
-            .places[0]
-        )
-    except ValidationError:
-        logger.exception("No matching places for %s", venue_full_name)
-        raise
-
-    return Venue(
-        # save the name without the city and state
-        name=venue_full_name.split(",", maxsplit=1)[0],
-        id=str(venue_id),
-        address=response.formattedAddress,
-        place_id=response.id,
-        location=response.location,
-    )
 
 
 if __name__ == "__main__":
@@ -47,7 +20,7 @@ if __name__ == "__main__":
     teams = Teams(
         teams=[
             Team(
-                id=str(team.id),
+                id=team.id,
                 name=team.name,
             )
             for team in mlb_teams.teams
@@ -76,11 +49,11 @@ if __name__ == "__main__":
     events = Events(
         events=[
             Event(
-                id=str(game.gamePk),
+                id=game.gamePk,
                 time=game.gameDate,
-                venue_id=str(game.venue.id),
-                home_team_id=str(game.teams.home.team.id),
-                away_team_id=str(game.teams.away.team.id),
+                venue_id=game.venue.id,
+                home_team_id=game.teams.home.team.id,
+                away_team_id=game.teams.away.team.id,
             )
             for date in mlb_schedule.dates
             for game in date.games
